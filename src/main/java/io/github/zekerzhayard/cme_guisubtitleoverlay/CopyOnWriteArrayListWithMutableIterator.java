9839576b4ee343681a959573101356a84db3d8e3
ZekerZhayard <trees1000@qq.com>
2023-01-22 15:30:56 +0800

    1.2

---
+++ src/main/java/io/github/zekerzhayard/cme_guisubtitleoverlay/CopyOnWriteArrayListWithMutableIterator.java
@@ -0,0 +1,145 @@
+package io.github.zekerzhayard.cme_guisubtitleoverlay;
+
+import java.lang.reflect.InvocationTargetException;
+import java.lang.reflect.Method;
+import java.util.Iterator;
+import java.util.ListIterator;
+import java.util.NoSuchElementException;
+import java.util.Objects;
+import java.util.concurrent.CopyOnWriteArrayList;
+import java.util.function.Consumer;
+
+public class CopyOnWriteArrayListWithMutableIterator<T> extends CopyOnWriteArrayList<T> {
+    static Method getArrayMethod = getMethod();
+
+    private static Method getMethod() {
+        try {
+            Method m = CopyOnWriteArrayList.class.getDeclaredMethod("getArray");
+            m.setAccessible(true);
+            return m;
+        } catch (NoSuchMethodException e) {
+            throw new RuntimeException(e);
+        }
+    }
+
+    protected Object[] getArray() {
+        try {
+            return (Object[]) getArrayMethod.invoke(this);
+        } catch (IllegalAccessException | InvocationTargetException e) {
+            throw new RuntimeException(e);
+        }
+    }
+
+    /**
+     * Returns an iterator over the elements in this list in proper sequence.
+     *
+     * <p>The returned iterator provides a snapshot of the state of the list
+     * when the iterator was constructed. No synchronization is needed while
+     * traversing the iterator. The iterator does <em>NOT</em> support the
+     * {@code remove} method.
+     *
+     * @return an iterator over the elements in this list in proper sequence
+     */
+    public Iterator<T> iterator() {
+        return new COWIterator<T>(getArray(), 0);
+    }
+
+    /**
+     * {@inheritDoc}
+     *
+     * <p>The returned iterator provides a snapshot of the state of the list
+     * when the iterator was constructed. No synchronization is needed while
+     * traversing the iterator. The iterator does <em>NOT</em> support the
+     * {@code remove}, {@code set} or {@code add} methods.
+     */
+    public ListIterator<T> listIterator() {
+        return new COWIterator<T>(getArray(), 0);
+    }
+
+    /**
+     * {@inheritDoc}
+     *
+     * <p>The returned iterator provides a snapshot of the state of the list
+     * when the iterator was constructed. No synchronization is needed while
+     * traversing the iterator. The iterator does <em>NOT</em> support the
+     * {@code remove}, {@code set} or {@code add} methods.
+     *
+     * @throws IndexOutOfBoundsException {@inheritDoc}
+     */
+    public ListIterator<T> listIterator(int index) {
+        Object[] elements = getArray();
+        int len = elements.length;
+        if (index < 0 || index > len)
+            throw new IndexOutOfBoundsException("Index: "+index);
+
+        return new COWIterator<T>(elements, index);
+    }
+
+    final class COWIterator<E> implements ListIterator<E> {
+        /** Snapshot of the array */
+        private final Object[] snapshot;
+        /** Index of element to be returned by subsequent call to next.  */
+        private int cursor;
+
+        private COWIterator(Object[] elements, int initialCursor) {
+            cursor = initialCursor;
+            snapshot = elements;
+        }
+
+        public boolean hasNext() {
+            return cursor < snapshot.length;
+        }
+
+        public boolean hasPrevious() {
+            return cursor > 0;
+        }
+
+        @SuppressWarnings("unchecked")
+        public E next() {
+            if (! hasNext())
+                throw new NoSuchElementException();
+            return (E) snapshot[cursor++];
+        }
+
+        @SuppressWarnings("unchecked")
+        public E previous() {
+            if (! hasPrevious())
+                throw new NoSuchElementException();
+            return (E) snapshot[--cursor];
+        }
+
+        public int nextIndex() {
+            return cursor;
+        }
+
+        public int previousIndex() {
+            return cursor-1;
+        }
+
+        public void remove() {
+            CopyOnWriteArrayListWithMutableIterator.this.remove(cursor);
+        }
+
+        @SuppressWarnings("unchecked")
+        public void set(E e) {
+            CopyOnWriteArrayListWithMutableIterator.this.set(cursor, (T) e);
+        }
+
+        @SuppressWarnings("unchecked")
+        public void add(E e) {
+            CopyOnWriteArrayListWithMutableIterator.this.add(cursor, (T) e);
+        }
+
+        @Override
+        public void forEachRemaining(Consumer<? super E> action) {
+            Objects.requireNonNull(action);
+            Object[] elements = snapshot;
+            final int size = elements.length;
+            for (int i = cursor; i < size; i++) {
+                @SuppressWarnings("unchecked") E e = (E) elements[i];
+                action.accept(e);
+            }
+            cursor = size;
+        }
+    }
+}
--- src/main/java/io/github/zekerzhayard/cme_guisubtitleoverlay/core/ClassTransformer.java
+++ src/main/java/io/github/zekerzhayard/cme_guisubtitleoverlay/core/ClassTransformer.java
@@ -2,40 +2,37 @@ package io.github.zekerzhayard.cme_guisubtitleoverlay.core;
 
 import net.minecraft.launchwrapper.IClassTransformer;
 import org.objectweb.asm.ClassReader;
-import org.objectweb.asm.ClassVisitor;
 import org.objectweb.asm.ClassWriter;
-import org.objectweb.asm.MethodVisitor;
 import org.objectweb.asm.Opcodes;
+import org.objectweb.asm.tree.AbstractInsnNode;
+import org.objectweb.asm.tree.ClassNode;
+import org.objectweb.asm.tree.InsnNode;
+import org.objectweb.asm.tree.MethodInsnNode;
+import org.objectweb.asm.tree.MethodNode;
+import org.objectweb.asm.tree.TypeInsnNode;
 
 public class ClassTransformer implements IClassTransformer {
     @Override
     public byte[] transform(String className, String transformedName, byte[] basicClass) {
         if ("net.minecraft.client.gui.GuiSubtitleOverlay".equals(transformedName)) {
-            ClassWriter cw = new ClassWriter(ClassWriter.COMPUTE_MAXS);
-            new ClassReader(basicClass).accept(new ClassVisitor(Opcodes.ASM5, cw) {
-                @Override
-                public MethodVisitor visitMethod(int access, String name, String desc, String signature, String[] exceptions) {
-                    MethodVisitor mv = super.visitMethod(access, name, desc, signature, exceptions);
-                    if (RemapUtils.checkMethodName(className.replace('.', '/'), name, desc, "<init>") && RemapUtils.checkMethodDesc(desc, "(Lnet/minecraft/client/Minecraft;)V")) {
-                        mv = new MethodVisitor(Opcodes.ASM5, mv) {
-                            @Override
-                            public void visitMethodInsn(int opcode, String owner, String name, String desc, boolean itf) {
-                                if (opcode == Opcodes.INVOKESTATIC && RemapUtils.checkClassName(owner, "com/google/common/collect/Lists") && RemapUtils.checkMethodName(owner, name, desc, "newArrayList") && RemapUtils.checkMethodDesc(desc, "()Ljava/util/ArrayList;")) {
-                                    this.visitTypeInsn(Opcodes.NEW, "java/util/Vector");
-                                    this.visitInsn(Opcodes.DUP);
-                                    opcode = Opcodes.INVOKESPECIAL;
-                                    owner = "java/util/Vector";
-                                    name = "<init>";
-                                    desc = "()V";
-                                    itf = false;
-                                }
-                                super.visitMethodInsn(opcode, owner, name, desc, itf);
+            ClassNode cn = new ClassNode();
+            new ClassReader(basicClass).accept(cn, ClassReader.EXPAND_FRAMES);
+            for (MethodNode mn : cn.methods) {
+                if (RemapUtils.checkMethodName(cn.name, mn.name, mn.desc, "<init>") && RemapUtils.checkMethodDesc(mn.desc, "(Lnet/minecraft/client/Minecraft;)V")) {
+                    for (AbstractInsnNode ain : mn.instructions.toArray()) {
+                        if (ain.getOpcode() == Opcodes.INVOKESTATIC) {
+                            MethodInsnNode min = (MethodInsnNode) ain;
+                            if (RemapUtils.checkClassName(min.owner, "com/google/common/collect/Lists") && RemapUtils.checkMethodName(min.owner, min.name, min.desc, "newArrayList") && RemapUtils.checkMethodDesc(min.desc, "()Ljava/util/ArrayList;")) {
+                                mn.instructions.insertBefore(min, new TypeInsnNode(Opcodes.NEW, "io/github/zekerzhayard/cme_guisubtitleoverlay/CopyOnWriteArrayListWithMutableIterator"));
+                                mn.instructions.insertBefore(min, new InsnNode(Opcodes.DUP));
+                                mn.instructions.set(min, new MethodInsnNode(Opcodes.INVOKESPECIAL, "io/github/zekerzhayard/cme_guisubtitleoverlay/CopyOnWriteArrayListWithMutableIterator", "<init>", "()V", false));
                             }
-                        };
+                        }
                     }
-                    return mv;
                 }
-            }, ClassReader.EXPAND_FRAMES);
+            }
+            ClassWriter cw = new ClassWriter(ClassWriter.COMPUTE_MAXS);
+            cn.accept(cw);
             basicClass = cw.toByteArray();
         }
         return basicClass;
